96faed184bdac6d605c2ba4600ca379614be58d3
Ahamed <ahamed.nasar@insurity.com>
2025-07-11 23:16:43 +0530

    added home page service menu verification

--- src/pages/HomePage.ts
+++ src/pages/HomePage.ts
@@ -1,10 +1,20 @@
-import {Page} from "@playwright/test";
+import {Page,expect} from "@playwright/test";
 
 export default class HomePage{
+
+    private readonly serviceTitleLocator = "Service";
 
     constructor ( private page : Page){
 
     }
 
+    async expectServiceTitleToBeVisible() {
+    await expect(this.page.getByTitle(this.serviceTitleLocator)).toBeVisible({
+      timeout: 15000,
+    }).catch((error) => {
+      console.error(`Error clicking login button: ${error}`);
+      throw error; // rethrow the error if needed
+    });
 
+    }
 }
