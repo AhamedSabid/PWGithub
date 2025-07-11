f311b085df42b2a379eb3eefc8009668483fcc99
Ahamed <ahamed.nasar@insurity.com>
2025-07-11 17:14:37 +0530

    yml file chnage and page close added in the tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,6 +5,7 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  await page.close();
 });
 
 test('get started link', async ({ page }) => {
@@ -15,4 +16,5 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.close();
 });
